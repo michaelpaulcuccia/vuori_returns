d11fd3d83eacc006bb2f33daa9e7ae62624ceb40
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2025-06-25 14:35:59 -0500

    progress capture on content

--- my-app/components/ReturnBar.tsx
+++ my-app/components/ReturnBar.tsx
@@ -9,21 +9,17 @@ const ReturnBar: React.FC = () => {
   return (
     <div className="w-full lg:w-[28%] p-4">
       <h2 className="headline-text">MAKING A RETURN?</h2>
-
       <p className="text-[#5a5a5a] font-bold">
         It's easy! Just enter your order information below to get started.
       </p>
-
       <p className="text-[#5a5a5a] font-bold">
         Returning a gift? Email us at support@vuori.com with your order number
         and the item you want to return.
       </p>
-
       <LinkText
         text="Need help finding your order number?"
         onClick={() => setShowHelpText(!showHelpText)}
       />
-
       {showHelpText && (
         <p className="mt-2 text-[#5a5a5a] text-sm leading-[19px]">
           Your order number is the seven or eight digit number included in your
@@ -31,8 +27,12 @@ const ReturnBar: React.FC = () => {
           slip in the 'Customer PO' line.
         </p>
       )}
-
-      <CustomForm />
+      <CustomForm />{" "}
+      <span className="text-[#5a5a5a] font-bold">
+        We apologize in advance as we are currently not offering exchanges. For
+        more information please interview our return policy.
+      </span>{" "}
+      <LinkText text="Our Return Policy" />
     </div>
   );
 };
